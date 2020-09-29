092aac0afed18c500592d5ca4d1819fa369750d5
Braian Emanuel Lopez <braian.lopez.ze@outlook.com>
2020-09-29 16:09:40 -0300

    First commit ⚡

---
+++ src/pages/Error404.js
@@ -0,0 +1,10 @@
+const Error404 = () => {
+    return  (
+        `
+            <div class="Error404">
+                <h2>Error404</h2>
+            </div>
+        `)
+}
+
+export default Error404;
