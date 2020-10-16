7be97f4284d6992886ea3a150da79c241292a4a3
Braian Lopez <braian.lopez.ze@outlook.com>
2020-10-16 20:01:13 -0300

    ready for github pages

--- src/template/Header.js
+++ src/template/Header.js
@@ -1,21 +1,21 @@
 const Header = () => {
-    const view = `
+  const view = `
             <div class="Header-main">
                 <div class="Header-logo">
                     <h1>
-                        <a href="/">
+                        <a href="/Rick-Morty/">
                             Rick&morty
                         </a>
                     </h1>
                 </div>
                 <div class="Header-nav>
-                    <a href="#/about">
+                    <a href="/Rick-Morty/#/about">
                         About
                     </a>
                 </div>
             </div>
         
-        `;
-    return view;
-};
-export default Header;
+        `
+  return view
+}
+export default Header
